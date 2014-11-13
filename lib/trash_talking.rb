51b89a9548a141c7391de1dd620aa5e12a446fdb
Orion Osborn <orion.osborn@gmail.com>
2014-11-13 11:43:33 -0700

    Removed Kates voice from TrashTalking module

--- lib/trash_talking.rb
+++ lib/trash_talking.rb
@@ -1,18 +1,18 @@
 module TrashTalking
   def trash_talk_too_short
-    system("say -v Kate 'Your guess was too small, like your brain'")
+    system("say 'Your guess was too small, like your brain'")
   end
 
   def trash_talk_congratulations
-    system("say -v Kate 'Congratulations.  We are all a little bit dumber for having watched you play.'")
+    system("say 'Congratulations.  We are all a little bit dumber for having watched you play.'")
   end
 
   def trash_talk_quit
-    system("say -v Kate 'Your parents were right, you are a quitter'")
+    system("say 'Your parents were right, you are a quitter'")
   end
 
   def trash_talk_quip
-    system("say -v Kate #{generate_quip}")
+    system("say #{generate_quip}")
   end
 
   def generate_quip
